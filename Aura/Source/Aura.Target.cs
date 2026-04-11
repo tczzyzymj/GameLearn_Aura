f05c901afec236d945b430dc91ad6223d01ef139
Michael_Yang <1460734808@qq.com>
2026-04-11 12:20:26 +0800

    ## Begin Udemy Course :Create a multiplayer RPG with Unreal Engine's Gameplay Ability System (GAS)

---
+++ Aura/Source/Aura.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class AuraTarget : TargetRules
+{
+	public AuraTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		ExtraModuleNames.Add("Aura");
+	}
+}
